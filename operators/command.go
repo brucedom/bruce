3fa73b999355124a0b01f1cfdf510d608a3636c1
Will Hattingh <w.hattingh@nitecon.com>
2023-10-04 14:39:17 -0400

    Commands should also handle envs

--- operators/command.go
+++ operators/command.go
@@ -13,10 +13,12 @@ type Command struct {
 	WorkingDir string `yaml:"dir"`
 	OsLimits   string `yaml:"osLimits"`
 	SetEnv     string `yaml:"setEnv"`
+	EnvCmd     string
 }
 
 func (c *Command) Setup() {
 	c.WorkingDir = RenderEnvString(c.WorkingDir)
+	c.EnvCmd = RenderEnvString(c.Cmd)
 }
 
 // Execute runs the command.
@@ -24,37 +26,37 @@ func (c *Command) Execute() error {
 	c.Setup()
 	/* We do not replace command envars like the other functions, this is intended to be a raw command */
 	if system.Get().CanExecOnOs(c.OsLimits) {
-		if len(c.Cmd) < 1 {
+		if len(c.EnvCmd) < 1 {
 			return fmt.Errorf("no command to execute")
 		}
-		logStr := c.Cmd
+		logStr := c.EnvCmd
 		if len(logStr) > 25 {
 			logStr = logStr[0:25] + "..."
 		}
 		log.Info().Msgf("executing command: %s", logStr)
-		fileName := exe.EchoToFile(c.Cmd, os.TempDir())
+		fileName := exe.EchoToFile(c.EnvCmd, os.TempDir())
 		// change directory to the working directory if specified
 		err := os.Chmod(fileName, 0775)
 		if err != nil {
 			log.Error().Err(err).Msg("temp file must exist to continue")
 			return err
 		}
-		log.Debug().Str("command", c.Cmd).Msgf("executing local file: %s", fileName)
+		log.Debug().Str("command", c.EnvCmd).Msgf("executing local file: %s", fileName)
 		pc := exe.Run(fileName, c.WorkingDir)
 		if pc.Failed() {
 			log.Error().Err(pc.GetErr()).Msg(pc.Get())
 			return pc.GetErr()
 		} else {
-			log.Debug().Str("cmd", c.Cmd).Msgf("completed executing: %s", fileName)
+			log.Debug().Str("cmd", c.EnvCmd).Msgf("completed executing: %s", fileName)
 			log.Debug().Msgf("Output: %s", pc.Get())
 			if len(c.SetEnv) > 0 {
-				log.Debug().Str("cmd", c.Cmd).Msgf("setting env var: %s=%s", c.SetEnv, pc.Get())
-				os.Setenv(c.SetEnv, pc.Get())
+				log.Debug().Str("cmd", c.EnvCmd).Msgf("setting env var: %s=%s", c.SetEnv, pc.Get())
+				log.Error().Err(os.Setenv(c.SetEnv, pc.Get()))
 			}
-			os.Remove(fileName)
+			log.Error().Err(os.Remove(fileName))
 		}
 	} else {
-		log.Info().Str("cmd", c.Cmd).Msgf("skipped due to os limit: %s", c.OsLimits)
+		log.Info().Str("cmd", c.EnvCmd).Msgf("skipped due to os limit: %s", c.OsLimits)
 	}
 	return nil
 }
